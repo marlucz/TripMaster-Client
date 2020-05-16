bc4fcfd2a8e8e79fd4b5f06ee57958ad8983f371
Bukacz <mbukacz@gmail.com>
2020-05-16 14:22:05 +0200

    add GlobalSpinner component

---
+++ src/components/GlobalSpinner/GlobalSpinner.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { color } from 'theme/GlobalStyle';
+
+export const StyledSpinnerOverlay = styled.div`
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const StyledSpinnerContainer = styled.div`
+    display: inline-block;
+    width: 50px;
+    height: 50px;
+    border: 3px solid ${color.grayLight};
+    border-radius: 50%;
+    border-top-color: ${color.grayDark};
+    animation: spin 1s ease-in-out infinite;
+    -webkit-animation: spin 1s ease-in-out infinite;
+    @keyframes spin {
+        to {
+            -webkit-transform: rotate(360deg);
+        }
+    }
+    @-webkit-keyframes spin {
+        to {
+            -webkit-transform: rotate(360deg);
+        }
+    }
+`;
+
+const GlobalSpinner = () => (
+    <StyledSpinnerOverlay>
+        <StyledSpinnerContainer />
+    </StyledSpinnerOverlay>
+);
+
+export default GlobalSpinner;
