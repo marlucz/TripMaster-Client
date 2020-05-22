e5e6ba2f7fb4f36c473f623a520007b068b5822d
Bukacz <mbukacz@gmail.com>
2020-05-22 19:49:54 +0200

    add itinerary redux types, add fetchItinerary and addItineraryItem actions

--- src/store/itinerary/itinerary.actions.js
+++ src/store/itinerary/itinerary.actions.js
@@ -1,5 +1,49 @@
 import ItineraryActionsTypes from 'store/itinerary/itinerary.types';
+import axios from 'axios';
 
-export const getItinerary = () => ({
-    type: ItineraryActionsTypes.GET_ITINERARY,
-});
+export const fetchItinerary = slug => (dispatch, getState) => {
+    dispatch({ type: ItineraryActionsTypes.FETCH_ITINERARY_REQUEST });
+
+    return axios
+        .get(`http://localhost:4000/api/trips/${slug}/itinerary`, {
+            params: {
+                userID: getState().user.currentUser.id,
+            },
+        })
+        .then(({ data }) => {
+            dispatch({
+                type: ItineraryActionsTypes.FETCH_ITINERARY_SUCCESS,
+                payload: data,
+            });
+        })
+        .catch(err =>
+            dispatch({
+                type: ItineraryActionsTypes.FETCH_ITINERARY_FAILURE,
+                payload: err,
+            }),
+        );
+};
+
+export const addItineraryItem = (itemContent, slug) => (dispatch, getState) => {
+    dispatch({
+        type: ItineraryActionsTypes.ADD_ITINERARY_ITEM_REQUEST,
+    });
+
+    return axios
+        .post(`http://localhost:4000/api/trips/${slug}`, {
+            userID: getState().user.currentUser.id,
+            ...itemContent,
+        })
+        .then(payload => {
+            dispatch({
+                type: ItineraryActionsTypes.ADD_ITINERARY_ITEM_SUCCESS,
+                payload,
+            });
+        })
+        .catch(err => {
+            dispatch({
+                type: ItineraryActionsTypes.ADD_ITINERARY_ITEM_FAILURE,
+                payload: err,
+            });
+        });
+};
