4ff7b2d6b3f5815b255793cb182642f89078194c
Bukacz <marcin_luczak@hotmail.com>
2020-05-23 15:11:26 +0200

    use reselect to get isAuth value from the state

--- src/hoc/withSession.js
+++ src/hoc/withSession.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { checkSession as checkSessionAction } from 'store/user/user.actions';
+import { selectIsAuth } from 'store/user/user.selectors';
 
 const withSession = Component => {
     const WithSession = ({ isAuth, checkSession }, ...props) => {
@@ -16,8 +18,9 @@ const withSession = Component => {
         checkSession: () => dispatch(checkSessionAction()),
     });
 
-    const mapStateToProps = ({ user: { isAuth = false } }) => ({ isAuth });
-
+    const mapStateToProps = createStructuredSelector({
+        isAuth: selectIsAuth,
+    });
     return connect(mapStateToProps, mapDispatchToProps)(WithSession);
 };
 
