e02a480671a34618cfdfb36b634ac3b57c05cb39
marlucz <marcin_luczak@hotmail.com>
2020-06-10 13:41:23 +0200

    add types, actions and build reducer for expenses

--- src/store/expenses/expenses.actions.js
+++ src/store/expenses/expenses.actions.js
@@ -1,5 +1,72 @@
 import ExpensesActionsTypes from 'store/expenses/expenses.types';
+import axios from 'axios';
 
-export const getExpenses = () => ({
-    type: ExpensesActionsTypes.GET_EXPENSES,
-});
+export const fetchExpenses = slug => (dispatch, getState) => {
+    dispatch({ type: ExpensesActionsTypes.FETCH_EXPENSES_REQUEST });
+
+    return axios
+        .get(`http://localhost:4000/api/trips/${slug}/expenses`, {
+            params: {
+                userID: getState().user.currentUser.id,
+            },
+        })
+        .then(({ data }) => {
+            dispatch({
+                type: ExpensesActionsTypes.FETCH_EXPENSES_SUCCESS,
+                payload: data,
+            });
+        })
+        .catch(err =>
+            dispatch({
+                type: ExpensesActionsTypes.FETCH_EXPENSES_FAILURE,
+                payload: err,
+            }),
+        );
+};
+
+export const addExpenseItem = itemContent => (dispatch, getState) => {
+    dispatch({
+        type: ExpensesActionsTypes.ADD_EXPENSE_ITEM_REQUEST,
+    });
+
+    const { slug } = getState().trips.activeTrip;
+
+    return axios
+        .post(`http://localhost:4000/api/trips/${slug}/expenses`, {
+            userID: getState().user.currentUser.id,
+            ...itemContent,
+        })
+        .then(payload => {
+            dispatch({
+                type: ExpensesActionsTypes.ADD_EXPENSE_ITEM_SUCCESS,
+                payload,
+            });
+        })
+        .catch(err => {
+            dispatch({
+                type: ExpensesActionsTypes.ADD_EXPENSE_ITEM_FAILURE,
+                payload: err,
+            });
+        });
+};
+
+export const removeItineraryItem = (slug, id) => dispatch => {
+    dispatch({ type: ExpensesActionsTypes.REMOVE_EXPENSE_ITEM_REQUEST });
+
+    return axios
+        .delete(`http://localhost:4000/api/trips/${slug}/expenses/${id}`)
+        .then(() => {
+            dispatch({
+                type: ExpensesActionsTypes.REMOVE_EXPENSE_ITEM_SUCCESS,
+                payload: {
+                    id,
+                },
+            });
+        })
+        .catch(err =>
+            dispatch({
+                type: ExpensesActionsTypes.REMOVE_EXPENSE_ITEM_FAILURE,
+                payload: err,
+            }),
+        );
+};
--- src/store/expenses/expenses.reducer.js
+++ src/store/expenses/expenses.reducer.js
@@ -1,91 +1,41 @@
 import ExpensesActionsTypes from 'store/expenses/expenses.types';
 
 const INITIAL_STATE = {
-    items: [
-        // {
-        //     id: 1,
-        //     date: '2020.04.28',
-        //     hour: '10:28',
-        //     name: 'Item 1',
-        //     tags: ['food', 'tickets', 'living'],
-        //     value: 10,
-        //     currency: 'USD',
-        // },
-        // {
-        //     id: 2,
-        //     date: '2020.04.28',
-        //     hour: '10:28',
-        //     name: 'Item 2',
-        //     tags: ['food', 'tickets', 'living'],
-        //     value: 10,
-        //     currency: 'CAD',
-        // },
-        // {
-        //     id: 3,
-        //     date: '2020.04.28',
-        //     hour: '10:28',
-        //     name: 'Item 3',
-        //     tags: ['food', 'tickets', 'living'],
-        //     value: 10,
-        //     currency: 'PLN',
-        // },
-        // {
-        //     id: 4,
-        //     date: '2020.04.28',
-        //     hour: '10:28',
-        //     name: 'Item 4',
-        //     tags: ['food', 'tickets', 'living'],
-        //     value: 10,
-        //     currency: 'AED',
-        // },
-        // {
-        //     id: 5,
-        //     date: '2020.04.28',
-        //     hour: '10:28',
-        //     name: 'Item 5',
-        //     tags: ['food', 'tickets', 'living'],
-        //     value: 10,
-        //     currency: 'AMD',
-        // },
-        // {
-        //     id: 6,
-        //     date: '2020.04.28',
-        //     hour: '10:28',
-        //     name:
-        //         'To jest jakiś bardzo długi tekst opisujący na co poszło tyle hajsu',
-        //     tags: ['food', 'tickets', 'living'],
-        //     value: 10,
-        //     currency: 'AZN',
-        // },
-        // {
-        //     id: 7,
-        //     date: '2020.04.28',
-        //     hour: '10:28',
-        //     name: 'Item 7',
-        //     tags: ['food', 'tickets', 'living'],
-        //     value: 10,
-        //     currency: 'CLP',
-        // },
-        // {
-        //     id: 8,
-        //     date: '2020.04.28',
-        //     hour: '10:28',
-        //     name: 'Item 8',
-        //     tags: ['food', 'tickets', 'living'],
-        //     value: 10,
-        //     currency: 'EGP',
-        // },
-    ],
+    items: [],
+    isLoading: false,
 };
 
 const expensesReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case ExpensesActionsTypes.GET_EXPENSES:
+        case ExpensesActionsTypes.FETCH_EXPENSES_REQUEST:
+        case ExpensesActionsTypes.ADD_EXPENSE_ITEM_REQUEST:
+        case ExpensesActionsTypes.REMOVE_EXPENSE_ITEM_REQUEST:
             return {
                 ...state,
-                expenses: state.items,
+                isLoading: true,
             };
-
+        case ExpensesActionsTypes.FETCH_EXPENSES_SUCCESS:
+            return {
+                ...state,
+                items: [...action.payload],
+                isLoading: false,
+            };
+        case ExpensesActionsTypes.ADD_EXPENSE_ITEM_SUCCESS:
+            return {
+                ...state,
+                items: [...state.items, action.payload.data],
+                isLoading: false,
+            };
+        case ExpensesActionsTypes.REMOVE_EXPENSE_ITEM_SUCCESS:
+            return {
+                ...state,
+                items: [
+                    ...state.items.filter(
+                        item => item._id !== action.payload.id,
+                    ),
+                ],
+                isLoading: false,
+            };
         default:
             return state;
     }
